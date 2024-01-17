15962cc70764de49d8e3f110d1c6c962ee824034
DESKTOP-8VTCRTV\JD <719609117@qq.com>
2024-01-17 19:36:39 +0800

    创建

---
+++ src/layouts/components/Footer/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="footer flx-center">
+    <a href="https://github.com/HalseySpicy" target="_blank"> 2022 © 志豪链云 By 志豪集团. </a>
+  </div>
+</template>
+
+<style scoped lang="scss">
+@import "./index.scss";
+</style>
