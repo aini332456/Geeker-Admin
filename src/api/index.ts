11eb352bf437d109369f830d6fe7f273204eded8
DESKTOP-8VTCRTV\JD <719609117@qq.com>
2024-01-19 09:53:13 +0800

    ignore

--- src/api/index.ts
+++ src/api/index.ts
@@ -14,7 +14,7 @@ export interface CustomAxiosRequestConfig extends InternalAxiosRequestConfig {
   cancel?: boolean;
 }
 
-const config = {
+const config: AxiosRequestConfig = {
   // 默认地址请求地址，可在 .env.** 文件中修改
   baseURL: import.meta.env.VITE_API_URL as string,
   // 设置超时时间
@@ -98,19 +98,35 @@ class RequestHttp {
   /**
    * @description 常用请求方法封装
    */
-  get<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+  get<T>(
+    url: string,
+    params?: object,
+    _object: Omit<CustomAxiosRequestConfig, "params" | "headers"> = {}
+  ): Promise<ResultData<T>> {
     return this.service.get(url, { params, ..._object });
   }
-  post<T>(url: string, params?: object | string, _object = {}): Promise<ResultData<T>> {
+  post<T>(
+    url: string,
+    params?: object | string,
+    _object: Omit<CustomAxiosRequestConfig, "params" | "headers"> = {}
+  ): Promise<ResultData<T>> {
     return this.service.post(url, params, _object);
   }
-  put<T>(url: string, params?: object, _object = {}): Promise<ResultData<T>> {
+  put<T>(
+    url: string,
+    params?: object,
+    _object: Omit<CustomAxiosRequestConfig, "params" | "headers"> = {}
+  ): Promise<ResultData<T>> {
     return this.service.put(url, params, _object);
   }
-  delete<T>(url: string, params?: any, _object = {}): Promise<ResultData<T>> {
+  delete<T>(
+    url: string,
+    params?: any,
+    _object: Omit<CustomAxiosRequestConfig, "params" | "headers"> = {}
+  ): Promise<ResultData<T>> {
     return this.service.delete(url, { params, ..._object });
   }
-  download(url: string, params?: object, _object = {}): Promise<BlobPart> {
+  download(url: string, params?: object, _object: Omit<CustomAxiosRequestConfig, "params" | "headers"> = {}): Promise<BlobPart> {
     return this.service.post(url, params, { ..._object, responseType: "blob" });
   }
 }
